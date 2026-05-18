00e86477a9a2aba529ab5b81aef29108941d3b55
sophia-naomi <sophia.piltz@escola.pr.gov.br>
2026-05-18 15:17:24 +0000

    aula18

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 for (let i =0; i <botoes.length; i++){
     botoes[i].onclick = function (){
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remover("ativo");
             textos[j].classList.remover("ativo");
         }
-        botoes[i].classList.add("ativo")
-        textos[j].classList.add("ativo")
+        botoes[j].classList.add("ativo");
+        textos[j].classList.add("ativo");
     }
 console.log(botoes);
 }
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo1 = Date("2026-12-28T23:59:59");
