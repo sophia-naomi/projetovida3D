e7ccd411d68351e1a640fc3d874931d3acb6ee6d
sophia-naomi <sophia.piltz@escola.pr.gov.br>
2026-05-25 15:17:40 +0000

    aula25

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
-for (let i =0; i <botoes.length; i++){
-    botoes[i].onclick = function (){
-        for(let j=0; j<botoes.length; j++){
-            botoes[j].classList.remover("ativo");
-            textos[j].classList.remover("ativo");
-        }
-        botoes[j].classList.add("ativo");
-        textos[j].classList.add("ativo");
-    }
-console.log(botoes);
-}
-const contadores = document.querySelectorAll("contador");
-const tempoObjetivo1 = Date("2026-12-28T23:59:59");
-
 // [PARTE 1 LIBERADA] - Controle de Abas
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
@@ -63,5 +47,4 @@ function calculaTempo(tempoObjetivo1) {
         + horas + " horas "
         + minutos + " minutos "
         + segundos + " segundos ";
-
 }
